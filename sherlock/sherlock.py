e5f7ad34cd5d18957c52d2e7d086d8b2e24c4325
Greg Baker <gregory.baker@hpe.com>
2023-02-13 18:25:08 -0600

    Initial commit

---
+++ sherlock/sherlock.py
@@ -0,0 +1,28 @@
+import click
+from pathlib import Path
+
+
+def main():
+    """Entry point for the sherlock application"""
+    print("Hello, World!")
+
+    class Sherlock(object):
+        def __init__(self, conf_dir=None, debug=False):
+            self.conf_dir = os.path.abspath(conf_dir or str(Path.home())
+
+def get_direct_endpoints():
+    pass
+
+def get_proxy_endpoints():
+    pass
+
+def async_read_endpoints(endpoints[]):
+    pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
